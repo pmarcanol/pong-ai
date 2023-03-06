6defb31d54449d6ff0dd2682b6a46f0822f7f168
Pablo Marcano <pablo1012@outlook.com>
2023-03-06 10:16:48 -0300

    Gravity, pillars, and game loop

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,154 @@
-import { Application, Graphics } from 'pixi.js'
+import {
+	Application,
+	Container,
+	Graphics,
+	// Rectangle,
+	// Sprite,
+	// Texture,
+} from 'pixi.js'
 
-const app = new Application({
-	view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480,
-})
+abstract class Element {
+	abstract loop(delta: number): void
+}
+class App {
+	app: Application
+	constructor() {
+		this.app = new Application({
+			view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
+			resolution: window.devicePixelRatio || 1,
+			autoDensity: true,
+			backgroundColor: 0x6495ed,
+			width: window.screen.width,
+			height: window.screen.height,
+			autoStart: true,
+		})
+	}
 
-const sprite = new Graphics()
-	.beginFill(0xffff00)
-	.lineStyle(5, 0xff0000)
-	.drawCircle(100, 100, 10)
-
-app.stage.addChild(sprite)
-
-let elapsed = 0.0
-function gameLoop(delta: number) {
-	elapsed += delta
-	gravity(sprite)
-	handleKeys(elapsed)
-	if (moving) {
-		sprite.y -= movementSize * 3
+	start() {
+		const game = new Game(this.app)
+		game.loop()
 	}
 }
 
-let moving = false
-let movementEnd = 0
-let movementDuration = 5
-function handleKeys(elapsed: number) {
-	if (elapsed > movementEnd) {
-		moving = false
+class Game extends Element {
+	app: Application
+	player?: Player
+	args: any
+	level?: Level
+	constructor(_app: Application) {
+		super()
+		this.app = _app
+		this.app.ticker.add(this.loop)
+		this.init()
 	}
-	if (keys['ArrowUp']) {
-		moving = true
-		movementEnd = elapsed + movementDuration
+
+	init() {
+		this.player = new Player(this)
+		this.player.init()
+		this.level = new Level(this)
+	}
+
+	loop(...args: any) {
+		this.args = args
+	}
+
+	addTicker(ticker: (delta: number) => void) {
+		this.app.ticker.add(ticker)
+	}
+	addElement(element: Container) {
+		this.app.stage.addChild(element)
 	}
 }
 
-const movementSize = 1
-let keys: Record<string, boolean> = {}
+class Level {
+	game: Game
+	pillars: Pillar[] = []
+	constructor(game: Game) {
+		this.game = game
+		this.game.addTicker(this.loop.bind(this))
+		this.init()
+	}
 
-function keyEventListener(e: KeyboardEvent) {
-	e.type == 'keydown'
-	keys[e.key] = e.type == 'keydown'
+	init() {
+		this.pillars.push(new Pillar(this.game))
+	}
+
+	loop() {
+		// this.pillars.push(new Pillar(this.game))
+	}
 }
 
-function gravity(sprite: Graphics) {
-	sprite.y += movementSize
+class Pillar {
+	game: Game
+	bottomGraphics: Graphics = new Graphics()
+	topGraphics: Graphics = new Graphics()
+	constructor(game: Game) {
+		this.game = game
+		this.game.addTicker(this.loop.bind(this))
+		this.init()
+	}
+
+	init() {
+		this.bottomGraphics = this.initRect()
+		this.topGraphics = this.initRect()
+		this.bottomGraphics.x = window.innerWidth
+		this.topGraphics.x = window.innerWidth
+
+		this.bottomGraphics.y = window.innerHeight - this.bottomGraphics.height
+		this.topGraphics.y = -200
+
+		this.game.addElement(this.bottomGraphics)
+		this.game.addElement(this.topGraphics)
+	}
+
+	initRect() {
+		return new Graphics()
+			.beginFill(0x000000)
+			.drawRect(0, 0, 100, window.innerHeight / 2)
+			.endFill()
+	}
+
+	loop() {
+		this.bottomGraphics.x -= 1
+		this.topGraphics.x -= 1
+	}
 }
 
-document.addEventListener('keydown', keyEventListener)
-document.addEventListener('keyup', keyEventListener)
+class Player extends Element {
+	game: Game
+	graphics = new Graphics()
+	velocity = 0
+	constructor(game: Game) {
+		super()
+		this.game = game
+	}
 
-app.ticker.add(gameLoop)
+	init() {
+		this.graphics = new Graphics()
+		this.graphics.beginFill(0x000000)
+		this.graphics.drawCircle(0, 0, 20)
+		this.graphics.endFill()
+		this.graphics.y = 100
+		this.graphics.x = 100
+		this.game.addElement(this.graphics)
+		this.game.addTicker(this.loop)
+		window.addEventListener('click', this.jump)
+	}
 
-// app.ticker.
+	loop = (delta: number): void => {
+		this.gravity(delta)
+	}
+
+	gravity(delta: number) {
+		this.velocity += 0.18
+		this.graphics.y += this.velocity * delta
+	}
+
+	jump = () => {
+		this.velocity = -8
+	}
+}
+
+const app = new App()
+
+app.start()
